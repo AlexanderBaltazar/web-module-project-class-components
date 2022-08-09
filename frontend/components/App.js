8c393ebc3411d0459b3a756ea0d786fcf0e5b2d9
Alexander <0.abaltazar16@gmail.com>
2022-08-09 00:52:35 -0400

    almost completed, have to make some adjustments

--- frontend/components/App.js
+++ frontend/components/App.js
@@ -1,22 +1,72 @@
 import React from 'react'
+import Form from './Form'
+import TodoList from './TodoList'
+
+const todos = [
+  {
+    name: 'Organize Garage',
+    id: 1528817077286, // could look different, you could use a timestamp to generate it
+    completed: false
+  },
+  {
+    name: 'Bake Cookies',
+    id: 1528817084358,
+    completed: false
+  }
+]
 
 export default class App extends React.Component {
+
+  constructor() {
+    super()
+
+    this.state = {
+      todos: todos
+    }
+  }
+
+  addItem = (e, item) => {
+    const newItem = {
+      name: item,
+      id: Date.now(),
+      completed: false
+    }
+    this.setState({
+      todos: [...this.state.todos, newItem]
+    })
+  }
+
+  clear = (e) => {
+    this.setState({
+      todos: this.state.todos.filter(item => !item.completed)
+    })
+  }
+
+  toggleItem = (itemId) => {
+    this.setState({
+      todos: this.state.todos.map(item => {
+        if (itemId === item.id) {
+          return {
+            ...item,
+            completed: !item.completed
+          }
+        }
+        return item
+      })
+    })
+  }
+
   render() {
+
+
     return (
       <div>
-        <h1>Todos</h1>
-        <ul>
-          <li>Learn React</li>
-          <li>Meal Prep Food</li>
-          <li>Workout</li>
-        </ul>
-
-        <form>
-          <input/>
-          <button>Add</button>
-        </form>
-
-        <button>Clear</button>
+        <Form addItem={this.addItem} />
+        <TodoList 
+          todos={this.state.todos}
+          toggleItem={this.toggleItem}
+        />
+        <button onClick={this.clear}>Clear</button>
       </div>
     )
   }
